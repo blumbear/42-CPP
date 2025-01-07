430327502591da93bac6766785f9d1e1162be034
Tom Taquet <ttaquet@c1r3p3.42lehavre.fr>
2025-01-07 16:39:21 +0100

    + | Added Makefile and start main and class phonebook

---
+++ CPP00/ex01/phonebook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+class phonebook
+{
+	private:
+		Contact	contact[8];
+	public:
+		PhoneBook;
+		~PhoneBook;
+		
+		Contact get_contact(int index);
+}
+#endif
