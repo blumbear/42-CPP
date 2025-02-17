2d55c4645ff54c7241632ec6afafa55058dfadc5
Hp de moa <taquettom3@gmail.com>
2025-02-17 16:42:29 +0100

    + | Added main and finish the exercice

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "The memory address of the string variable	: " << &str << '\n';
+	std::cout << "The memory address held by stringPTR		: " << stringPTR << '\n';
+	std::cout << "The memory address held by stringREF		: " << &stringREF << '\n';
+
+	std::cout << "---------------------------------" << '\n';
+
+	std::cout << "The value of the string variable		: " << str << '\n';
+	std::cout << "The value pointed by stringPTR			: " << *stringPTR << '\n';
+	std::cout << "The value pointed by stringREF			: " << stringREF << '\n';
+}
