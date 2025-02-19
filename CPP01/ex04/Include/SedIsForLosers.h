0977880f735e0eea375e3a3e6265a24b0182a2fc
Hp de moa <taquettom3@gmail.com>
2025-02-19 18:17:27 +0100

    + | Added and finish ex04

---
+++ CPP01/ex04/Include/SedIsForLosers.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#define USAGE_ERROR "Usage -> ./SedIsForLosers <file_name> <str1> <str2>"
+#define FILE_ERROR "Error -> File doesn't exist or can't be read"
+#define NFILE_ERROR "Error -> The new file can't be open"
---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,42 @@
+#include "SedIsForLosers.h"
+
+int	print_error(std::string Error)
+{
+	std::cout << Error << '\n';
+	return (0);
+}
+
+std::string replaceFunction(std::string fileContent, std::string str1, std::string str2)
+{
+	size_t	str1Pos = fileContent.find(str1);
+	std::string res;
+
+	while (str1Pos != std::string::npos)
+	{
+		res.append(fileContent, 0, str1Pos);
+		fileContent.erase(0, str1Pos + str1.size());
+		res += str2;
+		str1Pos = fileContent.find(str1);
+	}
+	res += fileContent;
+	return res;
+}
+
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+		return (print_error(USAGE_ERROR));
+	std::ifstream infile(av[1]);
+	if (!infile.is_open())
+		return (print_error(FILE_ERROR));
+	std::string newFileName = std::string(av[1]) + ".replace";
+	std::ofstream newFile(newFileName.c_str());
+	if (!newFile.is_open())
+		return (print_error(NFILE_ERROR));
+	std::stringstream buffer;
+	buffer << infile.rdbuf();
+	std::string fileContent = buffer.str();
+	newFile << replaceFunction(fileContent, std::string(av[2]), std::string(av[3]));
+	return 1;
+}
