7b91174dafd4aea1f4ef682cb3e1c631f5a1ca71
Hp de moa <taquettom3@gmail.com>
2025-07-23 19:26:37 +0200

    ~ | Updated to parse data

--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,8 +6,13 @@
 #include <map>
 #include <cstdlib>
 #include <sstream>
+#include <iomanip>  // pour std::fixed et std::setprecision
 
-#define DELIMITER "|"
+#define BISEXTILE(Value) ((Value % 4 == 0 && Value % 100 != 0) || (Value % 4 == 0 && Value % 400 == 0))
+
+#define INPUT_DELIMITER "|"
+#define DATA_DELIMITER ","
+#define DATE_DELIMITER "-"
 #define DATA_BASE "data.csv"
 
 #define ERROR_FLAG "Error: "
@@ -17,3 +22,4 @@
 #define INPUT_ERROR "bad input => "
 #define VALUETOOLOW_ERROR "not a positive number."
 #define VALUETOOHIGHT_ERROR "number greater than 1000."
+#define DBPARSE_ERROR "Failed to parse the database."
--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,33 +1,115 @@
 #include "BitcoinExchange.hpp"
 
-// std::map<std::string, float> parseDb(std::string) {
+void	printDb(std::map<std::string, float> DB) {
+	std::map<std::string, float>::iterator it;
+	for (it = DB.begin(); it != DB.end(); it++) {
+		std::cout << it->first << "," << it->second << std::endl;
+	}
+}
 
-// }
+std::map<std::string, float> parseDb() {
+	std::ifstream	file(DATA_BASE);
+	std::string	line;
+	std::map<std::string, float> DB;
+
+	if (file.is_open() == false)
+		throw false;
+	
+	getline(file, line); // skip header
+
+	while (getline(file, line)) {
+
+		size_t sepPos = line.find(DATA_DELIMITER);
+
+		if (sepPos != std::string::npos) {
+
+			std::string date = line.substr(0, sepPos);
+			std::stringstream ss(line.substr(sepPos + 1));
+
+			float value;
+			if (!(ss >> value)) {
+				std::cerr << ERROR_FLAG << INPUT_ERROR << line << std::endl;
+				throw false;
+			}
+
+			int year = 0;
+			int month = 0;
+			int day = 0;
+			sepPos = date.find(DATE_DELIMITER);
+			year = atoi(date.substr(0, sepPos).c_str());
+			day = date.find_last_of(DATE_DELIMITER);
+			month = atoi(date.substr(sepPos + 1, day).c_str());
+			sepPos = day;
+			day = 0;
+			day = atoi(date.substr(sepPos + 1).c_str());
+			if (month > 12 || month < 1 || (year < 2009 && month == 1 && day < 3) || day > 31 || day < 0)
+				throw false;
+			DB.insert(std::pair<std::string, int>(date, value));
+		}
+	}
+	return (DB);
+}
+
+float findDate(std::map<std::string, float> DB) {
+	
+}
+
+void printLineResult(std::string date, float value, std::map<std::string, float> DB) {
+	int year = 0;
+	int month = 0;
+	int day = 0;
+	size_t sepPos = date.find(DATA_DELIMITER);
+
+	if (sepPos == std::string::npos)
+		throw false;
+	year = atoi(date.substr(0, sepPos).c_str());
+	day = date.find_last_of(DATE_DELIMITER);
+	month = atoi(date.substr(sepPos + 1, day).c_str());
+	sepPos = day;
+	day = atoi(date.substr(sepPos + 1).c_str());
+	if (day < 1 || day > 31 || (BISEXTILE(year) && month == 2 && day > 29) || (!BISEXTILE(year)) && month == 2 && day > 28)
+		throw false;
+	std::cout << date << " => " << value << " = " << findDate(DB) ;
+}
 
 bool parse(char *_file) {
-	// std::map<std::string, float> DB = parseDb(DATA_BASE);
+	std::map<std::string, float> DB;
 	std::ifstream	file(_file);
 	std::string		line;
 
+	try{
+		DB = parseDb();
+	}
+	catch (bool e) {
+		std::cout << ERROR_FLAG << DBPARSE_ERROR << std::endl;
+		return (false);
+	}
 	if (file.is_open() == false) {
 		std::cout << ERROR_FLAG << FILE_ERROR << std::endl;
 		return (false);
 	}
+
+	getline(file, line); // skip header
+
 	while (getline(file, line)) {
-		size_t pos = line.find(DELIMITER);
-		if (pos != std::string::npos) {
-			std::string date = line.substr(0, pos);
+		size_t sepPos = line.find(INPUT_DELIMITER);
+		if (sepPos != std::string::npos) {
+			std::string date = line.substr(0, sepPos);
+			std::stringstream ss(line.substr(sepPos + 1));
 
-			std::stringstream ss(line.substr(pos + 1));
 			float value;
-			ss >> value;
-
-			if (value < 0)
+			if (!(ss >> value))
+				std::cerr << ERROR_FLAG << INPUT_ERROR << line << std::endl;
+			else if (value < 0)
 				std::cout << ERROR_FLAG << VALUETOOLOW_ERROR << std::endl;
 			else if (value > 1000)
 				std::cout << ERROR_FLAG << VALUETOOHIGHT_ERROR << std::endl;
-			else
-				std::cout << date << DELIMITER << value << "l" << std::endl;
+			else {
+				try { printLineResult(date, value, DB); }
+				catch(bool e) {
+					std::cout << ERROR_FLAG << INPUT_ERROR << line << std::endl;
+				}
+			}
 		}
 		else {
 			std::cout << ERROR_FLAG << INPUT_ERROR << line << std::endl;
@@ -47,3 +129,13 @@ int main(int ac, char **av) {
 
 	return (EXIT_SUCCESS);
 }
+
+// int main() {
+// 	std::cout << BISEXTILE(2200) << std::endl;
+// 	std::cout << BISEXTILE(1976) << std::endl;
+// 	std::cout << BISEXTILE(1950) << std::endl;
+// 	std::cout << BISEXTILE(1900) << std::endl;
+// 	std::cout << BISEXTILE(2000) << std::endl;
+// 	std::cout << BISEXTILE(1967) << std::endl;
+// 	std::cout << BISEXTILE(2015) << std::endl;
+// }
