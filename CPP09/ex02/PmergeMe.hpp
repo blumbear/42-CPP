6a8b0e8bb8598ec10631db2f9d3ddb195caa50d5
Hp de moa <taquettom3@gmail.com>
2025-07-24 16:24:42 +0200

    + | Created files and check basic

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+
+#define ERROR_PROMPT "Error: "
+
+#define USAGE_ERROR "Usage: ./PmergeMe <n1> <n> <n3> <n4> <n4> ..."
---
+++ CPP09/ex02/main.cpp
@@ -0,0 +1,9 @@
+#include "PmergeMe.hpp"
+
+int main(int ac, char **av) {
+	if (ac < 2) {
+		std::cerr << ERROR_PROMPT << USAGE_ERROR << std::endl;
+	}
+	
+	(void)av;
+}
