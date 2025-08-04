85665bc0f985f683317cd148f33095307198e576
Tom Taquet <ttaquet@c1r2p1.42lehavre.fr>
2025-08-04 14:09:33 +0200

    ~ | Improved main

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,13 +6,13 @@ int main() {
 		Bureaucrat michel = Bureaucrat("michel", 1);
 		std::cout << std::endl;
 		std::cout << bob << std::endl << michel << std::endl << std::endl;
-		bob = michel;
-		std::cout << bob << std::endl << michel << std::endl << std::endl;
 		bob.incrementGrade();
 		std::cout << bob << std::endl << michel << std::endl << std::endl;
 		bob.incrementGrade();
 		std::cout << bob << std::endl << michel << std::endl << std::endl;
 		michel.decrementGrade();
+		std::cout << bob << std::endl << michel << std::endl << std::endl;
+		bob = michel;
 		std::cout << bob << std::endl << michel << std::endl << std::endl;
 	}
 	catch (const std::exception& e) {
