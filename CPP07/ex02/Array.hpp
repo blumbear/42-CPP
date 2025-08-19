1ae7a18f0ec5b04802b4ae5a560e90f8cca52964
Tom Taquet <ttaquet@c1r5p10.42lehavre.fr>
2025-08-19 12:48:34 +0200

    + | Added ex02

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -1,20 +1,42 @@
 #pragma once
 
+#include <exception>
+#include <string>
+#include <iostream>
+
 template <typename T> class Array {
 	private :
-		T *array;
+		T *_array;
+		unsigned int _size;
 	
 	public :
 /* ================= Canonical Form ================= */
-		Array();
-		Array(unsigned int n);
-		Array(const Array &other);
-		~Array();
-		Array &operator=(const Array &other);
+		Array(): _array(NULL), _size(0) {}
+		Array(unsigned int n) : _array(new T[n]), _size(n) {}
+		Array(const Array &other) : _array(new T[other._size]), _size(other._size) {
+			for (unsigned int i = 0; i < _size; i++) {_array[i] = other._array[i];}
+		}
+		~Array() {}
+		Array &operator=(const Array &other) {
+			if(this != &other){
+				delete _array;
+				_size = other._size;
+				_array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++){_array[i] = other._array[i];}
+			} return *this;
+		}
 
 /* ================= Other Function ================= */
 
-		unsigned int size() const;
-};
+		unsigned int size() const {return _size;}
 
-#include "Array.tpp"
+/* =============== Overload Operator =============== */
+		T	&operator[](unsigned int i) {return (i >= this->_size? throw Array::OutOfBounds() : (_array[i]));}
+		T const &operator[](unsigned int i)const {return (i >= this->_size? throw Array::OutOfBounds() : (_array[i]));}
+
+		class OutOfBounds : public std::exception
+		{
+			public:
+				virtual const char *what() const throw() {return "Error : Out of Bound";}
+		};
+};
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,5 +1,51 @@
 #include "Array.hpp"
 
 int main() {
-	
+	{
+		std::cout << "\033[32m" << std::endl << "Empty Array" << "\033[0m" << std::endl;
+		Array<int> emptyArray;
+		std::cout << "\033[32m" << std::endl << "Out Of Bounds" << "\033[0m" << std::endl;
+		try
+		{
+			std::cout << "emptyArray[5]: " << emptyArray[5] << std::endl;
+		}
+		catch(std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+
+	{
+		std::cout << "\033[32m" << std::endl << "Int Array" << "\033[0m" << std::endl;
+		Array<int> intArray(5);
+		for (int i = 0; i < 5; i++)
+			intArray[i] = i;
+		std::cout << "intArray Size: " << intArray.size() << std::endl;
+		std::cout << "intArray: ";
+		for (int i = 0; i < 5; i++)
+			std::cout << intArray[i] << " ";
+		std::cout << std::endl;
+
+		std::cout << "\033[32m" << std::endl << "Int Array Copy" << "\033[0m" << std::endl;
+		Array<int> intArrayCopy(intArray);
+		std::cout << "intArrayCopy: ";
+		for (int i = 0; i < 5; i++)
+			std::cout << intArrayCopy[i] << " ";
+		std::cout << std::endl;
+
+	}
+
+	{
+		std::cout << "\033[32m" << std::endl << "Double Array" << "\033[0m" << std::endl;
+	Array<double> doubleArray(10);
+	for (int i = 0; i < 10; i++)
+		doubleArray[i] = i / 2.0;
+	std::cout << "doubleArray size: " << doubleArray.size() << std::endl;
+	std::cout << "doubleArray: ";
+	for (int i = 0; i < 10; i++)
+		std::cout << doubleArray[i] << " ";
+	std::cout << std::endl;
+	}
+	return (0);
 }
