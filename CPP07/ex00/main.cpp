89307e51ebe4c1f418c95556e667910635faec78
Blumbear <142356574+blumbear@users.noreply.github.com>
2025-08-19 12:06:42 +0200

    + | Added subject test

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -3,6 +3,21 @@
 #include <string>
 
 int main() {
+	std::cout << "---- Subject ----" << std::endl;
+	int a = 2;
+	int b = 3;
+	swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << "---- Other ----" << std::endl;
 	std::cout << min(8,5) << std::endl;
 	std::cout << min(5,8) << std::endl;
 	std::cout << min(-25,25) << std::endl;
@@ -24,6 +39,5 @@ int main() {
 
 	swap(tmpa, tmpb);
 	std::cout << tmpa << " " << tmpb << std::endl;
-
 	return (1);
 }
