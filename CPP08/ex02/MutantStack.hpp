f37dcbb227aed771d48c5db6755b04b0ad959119
Hp de moa <taquettom3@gmail.com>
2025-09-05 19:49:41 +0200

    - | makefclean

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -8,21 +8,21 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-public:
-	typedef std::stack<T> _stack;
-	typedef typename std::stack<T>::container_type::iterator iterator;
+	public:
+		typedef std::stack<T> _stack;
+		typedef typename std::stack<T>::container_type::iterator iterator;
 
-	MutantStack() : _stack() {}
-	MutantStack(const MutantStack &other) : _stack(other) {}
-	~MutantStack() {}
+		MutantStack() : _stack() {}
+		MutantStack(const MutantStack &other) : _stack(other) {}
+		~MutantStack() {}
 
-	MutantStack &operator=(const MutantStack &other) {
-		if (this != &other)
-			_stack::operator=(other);
-		return *this;
-	}
+		MutantStack &operator=(const MutantStack &other) {
+			if (this != &other)
+				_stack::operator=(other);
+			return *this;
+		}
 
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
 
 };
