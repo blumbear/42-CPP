825518f6290b877b5a2eb0d6ac0a32e849b760fd
Blumbear <142356574+blumbear@users.noreply.github.com>
2025-09-29 14:17:15 +0200

    Update main.cpp

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -14,7 +14,7 @@ int main(int ac, char **av) {
 		std::cout << "After: ";
 		data.printVector();
 		std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector : " << std::fixed<< std::setprecision(5) << data.getvtime() << " us" << std::endl;
-		std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector : " << std::fixed<< std::setprecision(5) << data.getdtime() << " us" << std::endl;
+		std::cout << "Time to process a range of " << ac - 1 << " elements with std::deque : " << std::fixed<< std::setprecision(5) << data.getdtime() << " us" << std::endl;
 
 	} catch (bool e) {
 		return (EXIT_SUCCESS);
